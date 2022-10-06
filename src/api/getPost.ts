ec74c3a1540bc7e9c0b3b06c1bc7cfe79fb0cfb3
Wiand1515 <marofual@gmail.com>
2022-10-06 12:25:08 -0300

    delete console.log

--- src/api/getPost.ts
+++ src/api/getPost.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getPosts = async (userID: number) => {
   const resp = await axios.get(
-    `https://gorest.co.in/public/v2/users/3335/posts`,
+    `https://gorest.co.in/public/v2/users/${userID}/posts`,
     {
       headers: {
         Authorization: `Bearer ${process.env.REACT_APP_GOREST_API_KEY}`,
