c4528653d71bd24ceee70139d4857bd3bebcc85d
Wiand1515 <marofual@gmail.com>
2022-10-06 11:43:58 -0300

    change name of env

--- src/api/deletePost.ts
+++ src/api/deletePost.ts
@@ -4,7 +4,7 @@ export const deletePost = async (postID:number) => {
   try {
     await axios.delete(`https://gorest.co.in/public/v2/posts/${postID}`, {
       headers: {
-        Authorization: `Bearer ${process.env.REACT_GOREST_API_KEY}`,
+        Authorization: `Bearer ${process.env.REACT_APP_GOREST_API_KEY}`,
       },
     });
   } catch (error) {
--- src/api/getPost.ts
+++ src/api/getPost.ts
@@ -1,14 +1,18 @@
 import axios from "axios";
 
 export const getPosts = async (userID: number) => {
+  console.log(userID)
+  
   const resp = await axios.get(
-    `https://gorest.co.in/public/v2/users/${userID}/posts`,
+    `https://gorest.co.in/public/v2/users/3335/posts`,
     {
       headers: {
-        Authorization: `Bearer ${process.env.REACT_GOREST_API_KEY}`,
+        Authorization: `Bearer ${process.env.REACT_APP_GOREST_API_KEY}`,
       },
     }
   );
+
+  console.log(resp.data);
 
   return resp.data;
 };
